909700d74cd08bf01bb190c679d4df0143c68783
andy henry <a.henry250@gmail.com>
2022-12-16 00:43:34 -0500

    preparing for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -149,5 +149,8 @@ AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_QUERYSTRING_AUTH = False
 AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 
 DEFAULT_FILE_STORAGE = 'portfolio.storage_backends.MediaStorage'
