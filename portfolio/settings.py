e2339cf784075790377e23a1c5c6da6c0987ed67
andy henry <a.henry250@gmail.com>
2023-06-25 16:50:33 -0400

    neccessary upgrades to the backend

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = ['127.0.0.1','www.andyhenry.net']
