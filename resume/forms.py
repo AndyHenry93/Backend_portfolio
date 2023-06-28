dc0eb8d4f935ceeb013eac2e9befd831fd6599ee
andy henry <a.henry250@gmail.com>
2023-06-28 10:43:11 -0400

    preparing for production

--- resume/forms.py
+++ resume/forms.py
@@ -2,10 +2,8 @@ from django import forms
 
 class ContactForm(forms.Form):
     email = forms.EmailField(label="Email")
-    subject = forms.CharField(max_length=150, label="subject")
     message = forms.CharField(widget=forms.Textarea,label="message")
 
     email.widget.attrs.update({'class':'form-control'})
-    subject.widget.attrs.update({'class':'form-control'})
     message.widget.attrs.update({'class':'form-control'})
     
