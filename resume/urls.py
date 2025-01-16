1e80d8485a4b5d6f5ba1067a8e1da4720278ac6d
Andy Henry <A.henry250@gmail.com>
2025-01-16 13:49:26 -0500

    updated template.

--- resume/urls.py
+++ resume/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('beta/',views.index, name='index'),
-    # path('detail/<int:pk>/', views.detail, name='detail'),
     path('', views.home, name='home'),
 ]
